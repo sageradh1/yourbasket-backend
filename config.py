c617cbb94219a832b4719147c2b956baacb2504b
Sagar <sageradh1@gmail.com>
2020-05-30 22:05:50 +0545

    Basic app

---
+++ config.py
@@ -0,0 +1,54 @@
+import secrets
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+    MAIL_SERVER = 'localhost'
+    MAIL_PORT = 25
+    MAIL_USERNAME = None
+    MAIL_PASSWORD = None
+
+    
+    # THUMBNAIL_FOR_UPLOADED_VIDEO_FOLDER = basedir+"/app/static/img/generated/thumbnails"
+    
+    # MAX_VIDEO_FILESIZE = 100 * 1024 * 1024 #max allowed video filesize is 16MB
+    # MAX_CSV_FILESIZE = 10 * 1024 * 1024 #max allowed csv filesize is 10MB
+
+    # BASE_URL_WITH_PORT = "http://127.0.0.1:5000"
+    # BASE_URL_WITH_PORT = "http://18.221.137.201"
+    # BASE_URL_WITH_PORT = "http://asmi.co"
+    
+    # ALLOWED_VIDEO_EXTENSIONS = set(['mp4', 'mkv'])
+    # ALLOWED_USERDATA_EXTENSIONS = set(['csv'])
+
+    SESSION_COOKIE_SECURE = True
+
+    SECRET_KEY = os.getenv('WEBAPP_SECRET_KEY')
+    
+class DevelopmentConfig(Config):
+    DEBUG = True
+    
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_SQLALCHEMY_DATABASE_URI')
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    DB_NAME = os.getenv('DEV_DB_NAME')
+    DB_USERNAME = os.getenv('DEV_DB_USERNAME')
+    DB_PASSWORD = os.getenv('DEV_DB_PASSWORD')
+
+    SESSION_COOKIE_SECURE = False
+
+class TestingConfig(Config):
+    TESTING = True
+
+    DB_NAME = os.getenv('TEST_DB_NAME')
+    DB_USERNAME = os.getenv('TEST_DB_USERNAME')
+    DB_PASSWORD = os.getenv('TEST_DB_PASSWORD')
+
+    SESSION_COOKIE_SECURE = False
+
+class ProductionConfig(Config):
+    pass
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/sagar/workingDir/asmi-dummy-project-backend/venv"
+set -gx VIRTUAL_ENV "/home/sagar/workingDir/yourbasket-services/yourbasket-backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
